9a27cd71fcff45520e864c5c5ca17b682ec63100
Angus-Read <angusread19@gmail.com>
2023-12-08 17:44:11 -0800

    band pass filter done

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,23 @@
-import scipy.signal import butter, sosfilt
+from scipy.signal import butter, sosfilt
+import pandas
+import matplotlib.pyplot as plt
 
 def main():
+    ecog_path = 'data/ECoG.csv'
+    motion_path = 'data/Motion.csv'
+    ecog_data = pandas.read_csv(ecog_path)
+    motion_data = pandas.read_csv(motion_path)
+
+    # print(ecog_data)
+    # print(motion_data)
+
+    
     # band pass filter from 0.1 to 600 Hz
+    for key in ecog_data.keys():
+        if key != 'ECoG_time':
+            ecog_data[key] = band_pass_filter(ecog_data[key])
+
+
     # re-reference using CAR montage
     # downsample motion markers to 20 Hz
     # Morlet DWT at 10 different freqs 10-150Hz in logarithmic scale
@@ -14,10 +30,28 @@ def main():
     
     return
 def band_pass_filter(data):
-    order = 1
-    Wn = [0.1, 600]
-    sos = butter(order, Wn, output='sos')
-    filtered = sosfilt(sos, data)
+    fs = 1000 # 1 kHz == 1000Hz sampling rate
+    nyq = 0.5 * fs # calculate nyquist 
+    low = 0.1 / nyq # 0.1 Hz low end of filter
+    high = 400 / nyq # should be 600 Hz high end of filter -- had to use 400 to get code working.
+
+    N = 3 # order
+    Wn = [low, high] # critical frequencies 
+    sos = butter(N, Wn, btype='bandpass', output='sos') # build filter
+
+    
+    batches = int(len(data) / fs)
+    filtered = []
+
+    for i in range(batches):
+        # apply filter over each second
+        batch_data = data[:fs]
+        data = data[fs:]
+        filtered += list(sosfilt(sos, batch_data)) 
+
+    # filter left-over data
+    filtered += list(sosfilt(sos, data))
+
     return filtered
 
 if __name__ == '__main__':
