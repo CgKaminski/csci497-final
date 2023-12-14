2dd074e0e898fe8ca900a9ba865819a9c5bcbd17
Harry Qiang <qiangh2@wwu.edu>
2023-12-13 18:48:23 -0800

    updated set split

--- pls.py
+++ pls.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import r2_score
 import numpy as np
 
 inputs = pd.read_csv('data/preprocessed_input.csv')
@@ -8,11 +10,28 @@ targets = pd.read_csv('data/targets.csv')
 X = inputs.values[:, 1:]
 Y = targets.values[:, 2:]
 
+val_idx = np.argmax(targets.values[:, 1] >= 600)
 
-pls = PLSRegression(n_components=21)
-pls.fit(X, Y)
+X_train = X[:val_idx, :]
+Y_train = Y[:val_idx, :]
+X_val = X[val_idx:, :]
+Y_val = Y[val_idx:, :]
 
-Y_pred = pls.predict(X)
+mse = []
+correlations = []
+n_components = np.arange(21, 22)
+for i in n_components:
+    pls = PLSRegression(n_components=i)
+    pls.fit(X_train, Y_train)
 
-correlation_coeffs = [np.corrcoef(Y[:, i], Y_pred[:, i])[0, 1] for i in range(Y.shape[1])]
-print('Correlation coefficients for each response variable:', correlation_coeffs)
+    Y_pred = pls.predict(X_train)
+    t_corr = [np.corrcoef(Y_train[:, j], Y_pred[:, j])[0, 1] for j in range(Y.shape[1])]
+    t_r2 = [r2_score(Y_train[:, j], Y_pred[:, j]) for j in range(Y.shape[1])]
+    print(t_corr)
+    print(t_r2)
+
+    Y_pred = pls.predict(X_val)
+    v_corr = [np.corrcoef(Y_val[:, j], Y_pred[:, j])[0, 1] for j in range(Y.shape[1])]
+    v_r2 = [r2_score(Y_val[:, j], Y_pred[:, j]) for j in range(Y.shape[1])]
+    print(v_corr)
+    print(v_r2)
