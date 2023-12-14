87ac152f9be11cff61bccba3ced09175be26437e
Cameron G. Kaminski <kaminsc@wwu.edu>
2023-12-14 12:05:15 -0800

    Made label font and tick size

--- graphs/coordinates_graph.py
+++ graphs/coordinates_graph.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import r2_score
 
 
-def calculate_correlation_coefficients(true_data: np.ndarray, pred_data: np.ndarray) -> list:
+def calculate_correlation_coefficients(true_data: np.ndarray[..., 3], pred_data: np.ndarray[..., 3]) -> list[float]:
     correlation_coefficients = [np.corrcoef(true_data[:, dim], pred_data[:, dim])[0, 1] for dim in range(3)]
     return correlation_coefficients
 
 
-def plot_coordinates(true_data: np.ndarray, pred_data: np.ndarray, total_time: int = 5,
+def plot_coordinates(true_data: np.ndarray[..., 3], pred_data: np.ndarray[..., 3], total_time: int = 5,
                      file_name: str = "plot") -> None:
     """
     Plots the coordinates of the true and predicted trajectories in 3 sub-plots (one for each dimension).
@@ -18,7 +17,6 @@ def plot_coordinates(true_data: np.ndarray, pred_data: np.ndarray, total_time: i
     :param file_name: The name of the file to save the plot to.
     :return: None.
     """
-    plt.clf()
     num_dimensions = 3
 
     fig, axs = plt.subplots(num_dimensions, 1, figsize=(6 * num_dimensions, 3 * num_dimensions))
@@ -36,39 +34,27 @@ def plot_coordinates(true_data: np.ndarray, pred_data: np.ndarray, total_time: i
 
             # Annotate the subplot with R-squared score
             axs[dim].text(0.9, 0.05, f'r : {correlation_coefficients[dim]:.2f}', transform=axs[dim].transAxes, ha='center',
-                          va='center')
+                          va='center', fontsize=14)
 
-        axs[dim].set_xlabel('Time (min)')
+            # Set tick label size
+            axs[dim].tick_params(axis='both', which='major', labelsize=14)
+
+        axs[dim].set_xlabel('Time (min)', fontsize=14)
 
     # Set y-axis label for each coordinate
     for dim, title in enumerate(['X-position (normalized)', 'Y-position (normalized)', 'Z-position (normalized)']):
-        axs[dim].set_ylabel(title)
+        axs[dim].set_ylabel(title, fontsize=14)
 
-    axs[0].legend(bbox_to_anchor=(1.0, 1.25), loc="upper right", frameon=False, ncol=1)
+    axs[0].legend(bbox_to_anchor=(1.0, 1.35), loc="upper right", frameon=False, fontsize=14, ncol=1)
 
     plt.tight_layout()
     plt.savefig(file_name)
 
-def plot_n_est(data, filename):
-    plt.clf()
-    x = range(1, len(data) +1)
-    plt.plot(x, data.T[0], label='X')
-    plt.plot(x, data.T[1], label='Y')
-    plt.plot(x, data.T[2], label='Z')
-    plt.legend()
-    plt.xlabel('Number of boosted trees')
-    plt.ylabel('Correlation Coefficient')
-    plt.tight_layout()
-    plt.savefig(filename)
-    print(data)
-    
+
 if __name__ == '__main__':
     # Load pred. data
     pls_data = np.genfromtxt('../output/PLS_pred.csv', delimiter=',', skip_header=1)
     xgb_data = np.genfromtxt('../output/XGB_pred.csv', delimiter=',', skip_header=1)
-
-    # load xgb n_est data
-    xgb_est_data = np.genfromtxt('../output/XGB_N_est.csv', delimiter=',')
 
     # Load motion data, extract last 3 cols. (x, y, z)
     motion_data = np.genfromtxt('../data/targets.csv', delimiter=',', skip_header=1)[-pls_data.shape[0]:, -3:]
@@ -76,4 +62,3 @@ if __name__ == '__main__':
     # Plot coordinates
     plot_coordinates(motion_data, pls_data, file_name='PLS_plot')
     plot_coordinates(motion_data, xgb_data, file_name='XGB_plot')
-    plot_n_est(xgb_est_data, 'XGB_N_est')
