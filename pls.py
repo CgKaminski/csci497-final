6c60e07683f7d13623e92452ebd74bc59e618e89
Harry Qiang <qiangh2@wwu.edu>
2023-12-13 17:52:43 -0800

    PLS initial

---
+++ pls.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.cross_decomposition import PLSRegression
+import numpy as np
+
+inputs = pd.read_csv('data/preprocessed_input.csv')
+targets = pd.read_csv('data/targets.csv')
+
+X = inputs.values[:, 1:]
+Y = targets.values[:, 2:]
+
+
+pls = PLSRegression(n_components=1)
+pls.fit(X, Y)
+
+Y_pred = pls.predict(X)
+
+correlation_coeffs = [np.corrcoef(Y[:, i], Y_pred[:, i])[0, 1] for i in range(Y.shape[1])]
+print('Correlation coefficients for each response variable:', correlation_coeffs)
