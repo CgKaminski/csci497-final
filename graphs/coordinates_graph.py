f77ee847a7aee99059e2a87d602451ede801f17d
CgKaminski <cameron.g.kaminski@gmail.com>
2023-12-14 02:02:25 -0800

    Changed input observation data

--- graphs/coordinates_graph.py
+++ graphs/coordinates_graph.py
@@ -22,15 +22,15 @@ def plot_coordinates(true_data: np.ndarray[..., 3], pred_data: np.ndarray[..., 3
     for i, (data, label, color) in enumerate(zip([true_data, pred_data], ['Observed', 'Predicted'], ['blue', 'red'])):
         for dim in range(num_dimensions):
             # Plot each dimension separately in mpl sub-plot
-            axs[dim].plot(time_values, data[:, dim], label=f'{label} trajectories {dim + 1}', color=color)
+            axs[dim].plot(time_values, data[:, dim], label=f'{label} trajectories', color=color, linewidth=1)
 
         axs[dim].set_xlabel('Time (min)')
 
     # Set y-axis label for each coordinate
-    for dim, title in enumerate(['X-position', 'Y-position', 'Z-position']):
+    for dim, title in enumerate(['X-position (normalized)', 'Y-position (normalized)', 'Z-position (normalized)']):
         axs[dim].set_ylabel(title)
 
-    axs[0].legend(bbox_to_anchor=(1.0, 1.15), loc="upper right", ncol=1)
+    axs[0].legend(bbox_to_anchor=(1.0, 1.15), loc="upper right", ncol=2)
 
     plt.tight_layout()
     plt.savefig(file_name)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     xgb_data = np.genfromtxt('../output/XGB_pred.csv', delimiter=',', skip_header=1)
 
     # Load motion data, extract last 3 cols. (x, y, z)
-    motion_data = np.genfromtxt('../data/Motion.csv', delimiter=',', skip_header=1)[-pls_data.shape[0]:, -3:]
+    motion_data = np.genfromtxt('../data/targets.csv', delimiter=',', skip_header=1)[-pls_data.shape[0]:, -3:]
 
     # Plot coordinates
     plot_coordinates(motion_data, pls_data, file_name='PLS_plot')
