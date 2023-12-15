1bc552a19088736111656d9e9dfac38ce34e5d08
CgKaminski <cameron.g.kaminski@gmail.com>
2023-12-14 19:55:20 -0800

    Changed corr coeff for press

--- pls_components.py
+++ pls_components.py
@@ -23,41 +23,44 @@ def load_data() -> tuple:
   return X, y, X_train, y_train, X_dev, y_dev
 
 
+from sklearn.metrics import mean_squared_error
+
 def pls_components(y, X_train, y_train, X_dev, y_dev, n_components: int) -> tuple:
-  """
-  Fit a PLS model with n_components and print the correlation and r2 scores for the training and validation sets.
-  :param y:
-  :param X_train:
-  :param y_train:
-  :param X_dev:
-  :param y_dev:
-  :param n_components:
-  :return:
-  """
+    """
+    Fit a PLS model with n_components and print the PRESS and r2 scores for the training and validation sets.
+    :param y:
+    :param X_train:
+    :param y_train:
+    :param X_dev:
+    :param y_dev:
+    :param n_components:
+    :return:
+    """
 
-  train_corr = np.zeros((n_components, y.shape[1]))
-  train_r2 = np.zeros((n_components, y.shape[1]))
-  val_corr = np.zeros((n_components, y.shape[1]))
-  val_r2 = np.zeros((n_components, y.shape[1]))
+    train_press = np.zeros((n_components, y.shape[1]))
+    train_r2 = np.zeros((n_components, y.shape[1]))
+    val_press = np.zeros((n_components, y.shape[1]))
+    val_r2 = np.zeros((n_components, y.shape[1]))
 
-  for i in range(1, n_components):
-    print(f'Fitting PLS with {i} components')
-    pls = PLSRegression(n_components=i)
-    pls.fit(X_train, y_train)
-    y_pred = pls.predict(X_train)
-    train_corr[i, :] = [np.corrcoef(y_train[:, j], y_pred[:, j])[0, 1] for j in range(y.shape[1])]
-    train_r2[i, :] = [r2_score(y_train[:, j], y_pred[:, j]) for j in range(y.shape[1])]
+    for i in range(1, n_components):
+        print(f'Fitting PLS with {i} components')
+        pls = PLSRegression(n_components=i)
+        pls.fit(X_train, y_train)
+        y_pred_train = pls.predict(X_train)
+        train_press[i, :] = [np.sum((y_train[:, j] - y_pred_train[:, j])**2) for j in range(y.shape[1])]
+        train_r2[i, :] = [r2_score(y_train[:, j], y_pred_train[:, j]) for j in range(y.shape[1])]
 
-    y_pred = pls.predict(X_dev)
-    val_corr[i, :] = [np.corrcoef(y_dev[:, j], y_pred[:, j])[0, 1] for j in range(y.shape[1])]
-    val_r2[i, :] = [r2_score(y_dev[:, j], y_pred[:, j]) for j in range(y.shape[1])]
+        y_pred_dev = pls.predict(X_dev)
+        val_press[i, :] = [np.sum((y_dev[:, j] - y_pred_dev[:, j])**2) for j in range(y.shape[1])]
+        val_r2[i, :] = [r2_score(y_dev[:, j], y_pred_dev[:, j]) for j in range(y.shape[1])]
 
-  return train_corr, train_r2, val_corr, val_r2
+    return train_press, train_r2, val_press, val_r2
+
 
 
 def main():
   X, y, X_train, y_train, X_dev, y_dev = load_data()
-  train_corr, train_r2, val_corr, val_r2 = pls_components(y, X_train, y_train, X_dev, y_dev, 100)
+  train_corr, train_r2, val_corr, val_r2 = pls_components(y, X_train, y_train, X_dev, y_dev, 30)
 
   # Save all the results together on a single csv
   results = np.concatenate((train_corr, train_r2, val_corr, val_r2), axis=1)
